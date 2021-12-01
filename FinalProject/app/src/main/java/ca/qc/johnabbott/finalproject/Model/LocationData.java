44d2d98004fc9cd5ab7a4207199f9323186e5122
Bhavik Patel <60612090+Bhavikbp13@users.noreply.github.com>
2021-11-30 19:27:07 -0500

    contact layout change and number change

--- FinalProject/app/src/main/java/ca/qc/johnabbott/finalproject/Model/LocationData.java
+++ FinalProject/app/src/main/java/ca/qc/johnabbott/finalproject/Model/LocationData.java
@@ -14,7 +14,7 @@ public class LocationData {
         data.add(new Location()
         .setAddress("21275 Rue Lakeshore Road, Sainte-Anne-de-Bellevue, QC H9X 3L9")
         .setHours("Open Hours\n   Mon:  8AM - 9PM\n   Tue:  8AM - 9PM\n   Wed:  8AM - 9PM\n   Thurs: 8AM - 9PM\n   Fri:   8AM - 9PM\n")
-        .setPhoneNumber("(514) 457-5036"));
+        .setPhoneNumber("(514) 233-5442"));
 
 //        data.add(new Location()
 //                .setAddress("12345 Rue Daniel Road, Sainte-Anne-de-Bellevue, QC H9X 3L9")
