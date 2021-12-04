493ba00eff3fc0fa9322e373a9d3e1d0c139c558
Mansi-b5 <60612351+Mansi-b5@users.noreply.github.com>
2021-12-04 18:03:26 -0500

    Detail View done
    Detail of an item UI part done

---
+++ FinalProject/app/src/main/java/ca/qc/johnabbott/finalproject/UI/MenuItem.java
@@ -0,0 +1,58 @@
+package ca.qc.johnabbott.finalproject.UI;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import ca.qc.johnabbott.finalproject.Model.Menu;
+import ca.qc.johnabbott.finalproject.Model.MenuData;
+import ca.qc.johnabbott.finalproject.R;
+
+public class MenuItem extends Fragment {
+
+
+    public MenuItem() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_menu_item, container, false);
+
+        TextView text = (TextView) view.findViewById(R.id.textItem);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public  void onViewCreated(View view, Bundle savedInstanceState)
+    {
+        super.onViewCreated(view,savedInstanceState);
+
+
+
+    }
+}
