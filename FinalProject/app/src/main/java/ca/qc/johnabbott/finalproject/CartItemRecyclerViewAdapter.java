023b237cf1ad6c330bf016a33180629b0636807a
Bhavik Patel <60612090+Bhavikbp13@users.noreply.github.com>
2021-12-14 21:26:45 -0500

    cart Item uses images linked to items

--- FinalProject/app/src/main/java/ca/qc/johnabbott/finalproject/CartItemRecyclerViewAdapter.java
+++ FinalProject/app/src/main/java/ca/qc/johnabbott/finalproject/CartItemRecyclerViewAdapter.java
@@ -57,7 +57,7 @@ public class CartItemRecyclerViewAdapter extends RecyclerView.Adapter<CartItemRe
 
         public void bind(CartItem cartItem, int position) {
             mItem = cartItem;
-            binding.imageView3.setImageResource(R.drawable.cart_placeholder_image);
+            binding.imageView3.setImageResource(cartItem.getMenuItem().getImageResourceId());
             binding.titleTextView.setText(cartItem.getMenuItem().getTitle());
             //binding.descriptionTextView.setText(cartItem.getProduct().getDescription());
             NumberFormat formatter = NumberFormat.getCurrencyInstance();
