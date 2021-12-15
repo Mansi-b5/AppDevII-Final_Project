bf822d117ba534c8e06ffbd5c949354e374dcf16
Mansi-b5 <60612351+Mansi-b5@users.noreply.github.com>
2021-12-15 04:22:41 -0500

    Details page not updating
    price and quantity not working

--- FinalProject/app/src/main/java/ca/qc/johnabbott/finalproject/UI/MenuDetails.java
+++ FinalProject/app/src/main/java/ca/qc/johnabbott/finalproject/UI/MenuDetails.java
@@ -7,11 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
 
+import ca.qc.johnabbott.finalproject.CartItemRecyclerViewAdapter;
+import ca.qc.johnabbott.finalproject.Model.CartItem;
 import ca.qc.johnabbott.finalproject.Model.MenuData;
 import ca.qc.johnabbott.finalproject.Model.Size;
 import ca.qc.johnabbott.finalproject.R;
@@ -48,12 +51,65 @@ public class MenuDetails extends Fragment {
 
 
         binding.title.setText(mainActivity.getOrderViewModel().getItem().getTitle());
+        //binding.quantityTextView.setText(mainActivity.getOrderViewModel().getItem().getQuantity());
         binding.imageView.setImageResource(mainActivity.getOrderViewModel().getItem().getImageResourceId());
         binding.description.setText(mainActivity.getOrderViewModel().getItem().getDescription());
+        //binding.quantityTextView.setText(mainActivity.getOrderViewModel().getItem().getQuantity());
 
 
-        binding.price.setText(String.valueOf(mainActivity.getOrderViewModel().getItem().getPrice()));
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Object item = adapterView.getItemAtPosition(i).toString();
+
+         
+                if(item.equals("MEDIUM"))
+                {
+                    binding.price.setText(String.valueOf(mainActivity.getOrderViewModel().getItem().getPrice() + 3.00));
+                    notifyViewModel();
+                }
+                else if(item.equals("LARGE")){
+                    binding.price.setText(String.valueOf(mainActivity.getOrderViewModel().getItem().getPrice() + 5.00));
+                    notifyViewModel();
+                }else{
+                    binding.price.setText(String.valueOf(mainActivity.getOrderViewModel().getItem().getPrice()));
+                    notifyViewModel();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
 
+
+//        binding.quantityMinusButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                int old = Integer.parseInt(binding.quantityTextView.getText().toString());
+//                if(old <= 1) {
+//                    return;
+//                }
+//                binding.quantityTextView.setText(--old);
+//            }
+//        });
+//
+//        binding.quantityPlusButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                int old = Integer.parseInt(binding.quantityTextView.getText().toString());
+//                binding.quantityTextView.setText(++old);
+//            }
+//        });
+
+
+    }
+
+    private void notifyViewModel() {
+
+        MainActivity mainActivity = (MainActivity) getActivity();
+        mainActivity.getOrderViewModel().notifyChange();
     }
 }
